b42b4cabfd2e7bf20e9df7285e906768183fac09
bbelly <bawei3600@gmail.com>
2022-06-20 21:39:35 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const imageList = new Array(164).fill('');
+
+  const levelList = [0, 29, 56, 83, 109, 136];
+
+  const levelName = ['火系', '水系', '电系', '毒虫', '草系', '格斗'];
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pikaster 技能介绍（中文）</h1>
+      <div className="content">
+        {imageList.map((image, index) => {
+          return (
+            <>
+              {levelList.includes(index) && (
+                <div class="level-content">
+                  <div>{levelName.shift()}</div>
+                </div>
+              )}
+              <img
+                src={`/images/pasted image 0(${index}).png`}
+                alt="pikaster-skill"
+              />
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
